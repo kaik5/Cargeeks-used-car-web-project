3cb7c54a618ff86eb186dac919a1c5a70635643e
kaik5 <kongjiajun555@outlook.com>
2021-11-07 09:36:36 -0700

    fourth

--- cargeeks/settings.py
+++ cargeeks/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cargeeks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cargeeks_db',
+        'USER': 'root@localhost',
+        'PASSWORD': 'Devil555.',
+        'HOST': 'localhost',
+
     }
 }
 
