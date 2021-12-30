dce0bc7b655b7125fd0808ea8b14cdd0c13829da
kaik5 <kongjiajun555@outlook.com>
2021-12-29 19:58:11 -0700

    database config and middleware (whitenoise)

--- cargeeks/settings.py
+++ cargeeks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+5l26#r2nhh3=3mpe8^@-t^=mld4a5eus8^xc1yl0*00(y0w72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -64,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cargeeks.urls'
@@ -90,17 +92,17 @@ WSGI_APPLICATION = 'cargeeks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'car_geeks_db',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'car_geeks_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
 
-    }
-}
-
+#     }
+# }
+DATABASE = {'default': dj_database_url.config(default = 'postgres://postgres:admin@localhost/car_geeks_db')}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -165,3 +167,6 @@ EMAIL_PORT = 587;
 EMAIL_HOST_USER = "kjjinfo555@gmail.com"
 EMAIL_HOST_PASSWORD = "devil555"
 EMAIL_USE_TLS = True;
+
+# whitenoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage';
